3433f34135a0c75c1c24e748b76befdcfb7d6562
Zhou, Haifeng <haifeng.zhou@accenture.com>
2017-05-11 22:55:18 +0800

    update info

--- app/src/main/java/chou/aric/com/vectordemo/MainActivity.java
+++ app/src/main/java/chou/aric/com/vectordemo/MainActivity.java
@@ -1,13 +1,29 @@
 package chou.aric.com.vectordemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public  void anim(View view){
+        Drawable drawable = ((ImageView) view).getDrawable();
+        if (drawable instanceof Animatable){
+            ((Animatable)drawable).start();
+        }
     }
 }
